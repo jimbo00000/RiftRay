b20f969d02bd3a411ea6ac25edca82718874747a
jimbo00000 <james.susinno@gmail.com>
2014-03-19 20:10:17 -0400

    Update the copyDLLs.py script to assemble both Debug and Release targets.

--- tools/copyDLLs.py
+++ tools/copyDLLs.py
@@ -10,21 +10,36 @@ import shutil
 # @todo Maybe grab this directory from CMakeCache
 dllHome = "C:/lib/"
 projectHome = "../"
-dllDest = projectHome + "build/Release/"
 
 # List of DLLs to copy. Paths are listed in components to easily accomodate
-# alternate configurations
+# alternate configurations.
 # @todo Use python's path handling library for correctness
 dllList = [
 	["AntTweakBar_116/" , "AntTweakBar/lib/"    , "AntTweakBar.dll"],
 	["glew/"            , "bin/"                , "glew32.dll"     ],
-	["SixenseSDK/"      , "bin/win32/debug_dll/"           , "sixensed.dll"  ],
-	["SixenseSDK/"      , "bin/win32/debug_dll/"           , "sixense_utilsd.dll"  ],
 	["SixenseSDK/"      , "samples/win32/sixense_simple3d/", "DeviceDLL.dll"  ],
 ]
 
-for f in dllList:
-    src = dllHome + f[0] + f[1] + f[2]
-    dst = dllDest + f[2]
-    print src,dst
-    shutil.copyfile(src, dst)
+debugDllList = [
+	["SixenseSDK/", "bin/win32/debug_dll/", "sixensed.dll"  ],
+	["SixenseSDK/", "bin/win32/debug_dll/", "sixense_utilsd.dll"  ],
+]
+
+releaseDllList = [
+	["SixenseSDK/", "bin/win32/release_dll/", "sixense.dll"  ],
+	["SixenseSDK/", "bin/win32/release_dll/", "sixense_utils.dll"  ],
+]
+
+def assembleBuild(buildname, dllHome, dllList):
+	""" Copy a list of dlls into a build directory.
+	"""
+	print "__Assembling: " + buildname
+	dllDest = projectHome + "build/" + buildname + "/"
+	for f in dllList:
+		src = dllHome + f[0] + f[1] + f[2]
+		dst = dllDest + f[2]
+		print "  copy\n    ",src,"\n    ",dst
+		shutil.copyfile(src, dst)
+
+assembleBuild("Debug", dllHome, dllList+debugDllList)
+assembleBuild("Release", dllHome, dllList+releaseDllList)
