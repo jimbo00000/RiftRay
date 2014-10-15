70d91397820b2640fa7f90367dd012c826db573d
jimbo00000 <james.susinno@gmail.com>
2014-10-14 22:03:19 -0400

    Add a directory traversal function to delve one level deeper into the shaders/ directory during shadertoy discovery.

--- src/Util/DirectoryFunctions.cpp
+++ src/Util/DirectoryFunctions.cpp
@@ -18,38 +18,75 @@ std::vector<std::string> GetListOfFilesFromDirectory(const std::string& d)
 {
     std::vector<std::string> names;
 
-    // Get a list of files in the directory
     DIR *dir;
     struct dirent *ent;
     if ((dir = opendir (d.c_str())) != NULL)
     {
-        printf("__List of shaders in %s:__\n", d.c_str());
-
-        /* print all the files and directories within directory */
         while ((ent = readdir (dir)) != NULL)
         {
-#if 0
-            // No lstat on Windows...
-            struct stat st;
-            lstat(ent->d_name, &st);
-            if (S_ISDIR(st.st_mode)
+            if (S_ISDIR(ent->d_type))
                 continue;
-#endif
-
             std::string s(ent->d_name);
-            if (s.length() < 5)
-                continue;
-
-            printf ("  %s\n", s.c_str());
             names.push_back(s);
         }
-        closedir (dir);
+        closedir(dir);
     }
     else
     {
-        /* could not open directory */
+        // could not open directory
         perror ("");
-        //return EXIT_FAILURE;
+    }
+
+    std::sort(names.begin(), names.end());
+    return names;
+}
+
+std::vector<std::string> GetListOfFilesFromDirectoryAndSubdirs(const std::string& d)
+{
+    std::vector<std::string> names;
+    std::vector<std::string> directories;
+
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir (d.c_str())) != NULL)
+    {
+        while ((ent = readdir (dir)) != NULL)
+        {
+            std::string s(ent->d_name);
+            if (!s.compare("."))
+                continue;
+            if (!s.compare(".."))
+                continue;
+            if (S_ISDIR(ent->d_type))
+            {
+                directories.push_back(s);
+                continue;
+            }
+            names.push_back(s);
+        }
+        closedir(dir);
+    }
+    else
+    {
+        // could not open directory
+        perror ("");
+    }
+
+    // Scan all subdirectories(only one level deep, not recursion)
+    for (std::vector<std::string>::const_iterator it = directories.begin();
+        it != directories.end();
+        ++it)
+    {
+        const std::string& subdir = *it;
+        std::vector<std::string> dirFiles = GetListOfFilesFromDirectory(d + subdir);
+        for (std::vector<std::string>::const_iterator it = dirFiles.begin();
+            it != dirFiles.end();
+            ++it)
+        {
+            const std::string& f = *it;
+            const std::string filepath = subdir + "/" + f;
+            names.push_back(filepath);
+        }
     }
 
     std::sort(names.begin(), names.end());
--- src/Util/DirectoryFunctions.h
+++ src/Util/DirectoryFunctions.h
@@ -4,3 +4,4 @@
 #include <string>
 
 std::vector<std::string> GetListOfFilesFromDirectory(const std::string& d);
+std::vector<std::string> GetListOfFilesFromDirectoryAndSubdirs(const std::string& d);
