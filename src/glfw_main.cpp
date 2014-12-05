b55af8325c554ae9024c6fdffbf17d38cfad6314
jimbo00000 <james.susinno@gmail.com>
2014-12-04 22:48:54 -0500

    Add key bindings for FBO scaling(F5-F8) to match jherico's OculusRiftInAction shadertoy app.

--- src/glfw_main.cpp
+++ src/glfw_main.cpp
@@ -112,6 +112,9 @@ void keyboard(GLFWwindow* pWindow, int key, int codes, int action, int mods)
         //printf("key ac  %d %d\n", key, action);
     }
 
+    const float f = 0.9f;
+    const float ff = 0.99f;
+
     if (action == GLFW_PRESS)
     {
     switch (key)
@@ -147,6 +150,11 @@ void keyboard(GLFWwindow* pWindow, int key, int codes, int action, int mods)
             g_renderMode.toggleRenderingTypeDistortion();
             LOG_INFO("Called toggleRenderingTypeDistortion");
             break;
+
+        case GLFW_KEY_F5: g_dynamicallyScaleFBO = false; g_app.SetFBOScale(f * g_app.GetFBOScale()); break;
+        case GLFW_KEY_F6: g_dynamicallyScaleFBO = false; g_app.SetFBOScale(ff * g_app.GetFBOScale()); break;
+        case GLFW_KEY_F7: g_dynamicallyScaleFBO = false; g_app.SetFBOScale((1.f/ff) * g_app.GetFBOScale()); break;
+        case GLFW_KEY_F8: g_dynamicallyScaleFBO = false; g_app.SetFBOScale((1.f/f) * g_app.GetFBOScale()); break;
 
         case '`':
             ///@todo Is there a way to create an auxiliary window in Direct to rift mode?
@@ -925,6 +933,8 @@ int main(void)
 
     glfwMakeContextCurrent(l_Window);
     g_pHMDWindow = l_Window;
+
+    SetVsync(1); // default to vsync on
 
     StartShaderLoad();
 
