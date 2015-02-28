e3524741bc97c1d7045af612c5669aaa3e052953
jimbo00000 <james.susinno@gmail.com>
2015-02-28 14:26:04 -0500

    Remove trailing zeros from 0.f.

--- src/AppSkeleton/RiftAppSkeleton.cpp
+++ src/AppSkeleton/RiftAppSkeleton.cpp
@@ -1126,7 +1126,7 @@ void RiftAppSkeleton::display_sdk() const
 
     bindFBO(m_renderBuffer);
 
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(0.f, 0.f, 0.f, 0.f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     ovrVector3f e2v[2] = {
