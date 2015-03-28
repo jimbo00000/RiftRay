722ecbf3c2a79bbab4c0a1c93a4788c2e5fb72a3
jimbo00000 <james.susinno@gmail.com>
2015-03-27 22:14:05 -0400

    Remove ovrDistortionCap_Chromatic flag, now permanently on in OVR SDK 0.5.0.1.

--- src/AppSkeleton/RiftAppSkeleton.cpp
+++ src/AppSkeleton/RiftAppSkeleton.cpp
@@ -406,7 +406,6 @@ int RiftAppSkeleton::ConfigureSDKRendering()
     m_Cfg.OGL.Header.Multisample = 0;
 
     const int distortionCaps =
-        ovrDistortionCap_Chromatic |
         ovrDistortionCap_TimeWarp |
         ovrDistortionCap_Vignette;
     ovrHmd_ConfigureRendering(m_Hmd, &m_Cfg.Config, distortionCaps, m_EyeFov, m_EyeRenderDesc);
@@ -421,7 +420,6 @@ int RiftAppSkeleton::ConfigureClientRendering()
         return 1;
 
     const int distortionCaps =
-        ovrDistortionCap_Chromatic |
         ovrDistortionCap_TimeWarp |
         ovrDistortionCap_Vignette;
     for (int eyeIndex = 0; eyeIndex < ovrEye_Count; eyeIndex++)
